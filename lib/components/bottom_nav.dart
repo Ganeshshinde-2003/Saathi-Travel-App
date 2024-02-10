7b1148ca9a24096bace80089900573817834f89e
Ganesh Shinde <sindheganesh2004@gmail.com>
2024-02-10 15:12:34 +0530

    Created the logout

--- lib/components/bottom_nav.dart
+++ lib/components/bottom_nav.dart
@@ -1,14 +1,18 @@
-// ignore_for_file: non_constant_identifier_names
+// ignore_for_file: non_constant_identifier_names, use_build_context_synchronously
 
 import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:myapp/components/snack_bar.dart';
 import 'package:myapp/pages/group_page.dart';
 import 'package:myapp/pages/home_page.dart';
+import 'package:myapp/pages/login_page.dart';
 import 'package:myapp/pages/profile_page.dart';
 import 'package:myapp/pages/search_page.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BottomNavigation extends StatefulWidget {
-  const BottomNavigation({super.key});
+  const BottomNavigation({Key? key}) : super(key: key);
 
   @override
   State<BottomNavigation> createState() => _BottomNavigationState();
@@ -17,6 +21,7 @@ class BottomNavigation extends StatefulWidget {
 class _BottomNavigationState extends State<BottomNavigation> {
   final navigationKey = GlobalKey<CurvedNavigationBarState>();
   int index = 0;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   final Screens = [
     const HomePageScreen(),
@@ -31,8 +36,9 @@ class _BottomNavigationState extends State<BottomNavigation> {
       const Icon(Icons.home_outlined, size: 30),
       const Icon(Icons.group_add_outlined, size: 30),
       const Icon(Icons.search, size: 30),
-      const Icon(Icons.person_2_outlined, size: 30),
+      const Icon(Icons.person_outline, size: 30),
     ];
+
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
@@ -41,12 +47,25 @@ class _BottomNavigationState extends State<BottomNavigation> {
           fit: BoxFit.contain,
         ),
         backgroundColor: const Color(0xFFEDEAEA),
+        iconTheme: const IconThemeData(
+          color: Color(0xFFDFBD43),
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.exit_to_app,
+              size: 25,
+            ),
+            onPressed: () => _showLogoutConfirmationDialog(context),
+          ),
+        ],
       ),
       extendBody: true,
       body: Screens[index],
       bottomNavigationBar: Theme(
-        data: Theme.of(context)
-            .copyWith(iconTheme: const IconThemeData(color: Colors.black)),
+        data: Theme.of(context).copyWith(
+          iconTheme: const IconThemeData(color: Colors.black),
+        ),
         child: CurvedNavigationBar(
           key: navigationKey,
           color: const Color(0xFFEDEAEA),
@@ -55,13 +74,54 @@ class _BottomNavigationState extends State<BottomNavigation> {
           items: items,
           height: 60,
           index: index,
-          onTap: (index) => setState(
-            () {
-              this.index = index;
-            },
-          ),
+          onTap: (index) => setState(() => this.index = index),
         ),
       ),
     );
+  }
+
+  void _showLogoutConfirmationDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Logout Confirmation'),
+          content: const Text('Do you really want to logout?'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('No'),
+            ),
+            TextButton(
+              onPressed: () {
+                _logoutAndNavigateToLogin();
+                Navigator.of(context).pop(); // Close the dialog
+              },
+              child: const Text('Yes'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _logoutAndNavigateToLogin() async {
+    try {
+      await _auth.signOut();
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginPage()),
+      );
+
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString('userCredential', "");
+    } catch (e) {
+      showSnackBar(
+        context: context,
+        content: e.toString(),
+      );
+    }
   }
 }
